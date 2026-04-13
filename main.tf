08655ea03bd15608be163a55f485c4e32378bc99
bvskarthik18 <bvskarthik18@gmail.com>
2026-04-13 22:47:43 +0530

    All files added

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-
+# Resource block to deploy ec2 Instance
 resource "aws_instance" "demo" {
     ami = "ami-0ea87431b78a82070"
     instance_type = "t2.micro"
