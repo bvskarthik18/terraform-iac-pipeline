ba5f2c845a42477aea64c2ad5071f80c1cde6061
bvskarthik18 <bvskarthik18@gmail.com>
2026-04-13 22:39:31 +0530

    tf files added

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+    ami = "ami-0ea87431b78a82070"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "jenkins-ci-demo"
+    }
+}
