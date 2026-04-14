3a47e4989e79dd40e1e4a0c86e535d5f45761e6b
bvskarthik18 <bvskarthik18@gmail.com>
2026-04-14 16:07:25 +0530

    TF code updated

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
 # Resource block to deploy ec2 Instance
 resource "aws_instance" "demo" {
     ami = "ami-0ea87431b78a82070"
-    instance_type = "t2.micro"
+    instance_type = var.instance_type
     tags = {
         Name = "jenkins-ci-demo"
     }
